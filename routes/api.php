e5bd547dea4500ec6c9a3bc3a8a3566c733ab2f6
Fatih Akın <fatihakin75@gmail.com>
2019-08-08 21:19:21 +0300

    Me eklendi

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,8 @@
 <?php
 
+use App\Http\Middleware\CheckAdminAndCompany;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,19 +19,24 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-
 Route::group([
-
-    'middleware' => 'auth:jwt',
-    'prefix' => 'auth'
-
-], function ($router) {
-
-
-    Route::post('logout', 'AuthController@logout');
-    Route::post('refresh', 'AuthController@refresh');
-    Route::post('me', 'AuthController@me');
-
+    'middleware' => 'auth:jwt'
+], function () {
+    Route::get('me', 'AuthController@me');
+    Route::get('logout', 'AuthController@logout');
 });
+
+//Route::group([
+//
+//    'middleware' => 'auth:jwt'
+//
+//], function ($router) {
+//
+//
+//    Route::post('logout', 'AuthController@logout');
+//    Route::post('refresh', 'AuthController@refresh');
+//    Route::get('me', 'AuthController@me');
+//
+//});
 
 Route::post('login', 'AuthController@login');

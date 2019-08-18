9019c7290239f2d82809a38488314bdf0188d0a9
Fatih Akın <fatihakin75@gmail.com>
2019-08-18 21:24:13 +0300

    Update Transformer.php

--- app/Lib/Transformer.php
+++ app/Lib/Transformer.php
@@ -1,16 +1,11 @@
 <?php
+
 namespace App\Lib;
 
-use App\Transformers\UserTransformer;
 use League\Fractal\Manager;
-use League\Fractal\Resource\Collection;
-use League\Fractal\Resource\Item;
 
 class Transformer extends Manager
 {
-
-    const ITEM = Item::class;
-    const COLLECTION = Collection::class;
     /**
      * Transformer constructor.
      */
@@ -19,12 +14,4 @@ class Transformer extends Manager
         parent::__construct();
         parent::setSerializer(new TransformerSerializer());
     }
-
-    public function getCreatedData($data){
-
-        return ((new Transformer())->createData(new Item($data, new UserTransformer())))->toArray();
-    }
-
-
-
 }
